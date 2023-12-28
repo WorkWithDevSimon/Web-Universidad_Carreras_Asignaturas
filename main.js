97672cf5c47739708840360ba54e28bd91432bed
Simon <simon.santibanez@inacapmail.cl>
2023-12-27 22:55:32 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// const carreras = ["Informática", "Enfermería"]
+// const asignaturasInfo = [
+//     {id: 1, nombre:"POO"},
+//     {id: 2, nombre:"BD"},
+//     {id: 3, nombre:"FrontEnd"}
+// ]
+// const asignaturasEnf = [
+//     {id: 1, nombre:"Inyecciones"},
+//     {id: 2, nombre:"Rompehuesos"},
+//     {id: 3, nombre:"Fracturas"}
+// ]
